daccfa05f0c873a5762d1e62924e0ca196540497
kolostring <yoerodriguezjunior@gmail.com>
2024-05-11 03:02:42 -0400

    feat: navbar scroll reactiveness

--- src/lib/components/Navbar.tsx
+++ src/lib/components/Navbar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Logo from "@/lib/assets/white-logo-79x19.svg"
 import Arrow from "@/lib/assets/arrow-rotated-10x11.svg"
+import useScroll from "../hooks/useScroll";
+import { useEffect, useRef} from "react";
 
 const links : {text: string, href: string}[] = [
   {text: "About", href: "/about"},
@@ -13,33 +15,52 @@ const links : {text: string, href: string}[] = [
 export default function Navbar(){
   const pathname = usePathname();
 
-  return <nav className="container flex items-center font-body mt-9">
-    <Link href={"/"} className="logo">
-      <Logo/>
-    </Link>
+  const currentScroll = useScroll();
+  const lastScrollRef = useRef<number>(0);
+  const scrollVisibilityPercentRef = useRef<number>(0);
 
-    <div className="flex uppercase gap-10 text-sm ml-auto">
-      {
-        links.map(({href, text}) => 
-          <Link 
-          href={href} 
-          key={href}
-          className={`flex flex-col ${pathname.includes(href) ? "text-design-accent" : ""}`}
-          >
-            <h3 className="block px-[2px] my-auto">{text}</h3>
-            {
-              pathname.includes(href) && 
-              <div className={"h-[2px] bg-design-accent"}></div>
-            }
-          </Link>
-        )
-      }
+  const navRef = useRef<HTMLElement>(null);
+  
+  useEffect(()=>{
+    const height = navRef.current?.getBoundingClientRect().height;
+    if(height){
+      const deltaTransform = (lastScrollRef.current-currentScroll)/height * 100;
+      const transform = scrollVisibilityPercentRef.current+deltaTransform;
 
-      <Link className="flex px-8 py-2 items-center gap-1 rounded-full font-medium text-design-background-primary bg-design-accent" href={"/lets-talk"}>
-        <h3>Let&apos;s talk</h3>
-        <Arrow height="1rem"/>
+      scrollVisibilityPercentRef.current = (Math.max(-100, Math.min(0, transform)));
+    }
+
+    lastScrollRef.current = currentScroll;
+  }, [currentScroll]);
+
+  return <nav ref={navRef} className={`w-full fixed bg-opacity-90 bg-design-background-primary backdrop-blur-sm`} style={{transform: `translate(0px, ${scrollVisibilityPercentRef.current}%)`}}>
+    <div className="container flex items-center font-body mt-9 mb-6">
+      <Link href={"/"} className="logo">
+        <Logo/>
       </Link>
+
+      <div className="flex uppercase gap-10 text-sm ml-auto">
+        {
+          links.map(({href, text}) => 
+            <Link 
+            href={href} 
+            key={href}
+            className={`flex flex-col ${pathname.includes(href) ? "text-design-accent" : ""}`}
+            >
+              <h3 className="block px-[2px] my-auto">{text}</h3>
+              {
+                pathname.includes(href) && 
+                <div className={"h-[2px] bg-design-accent"}></div>
+              }
+            </Link>
+          )
+        }
+
+        <Link className="flex px-8 py-2 items-center gap-1 rounded-full font-medium text-design-background-primary bg-design-accent" href={"/lets-talk"}>
+          <h3>Let&apos;s talk</h3>
+          <Arrow height="1rem"/>
+        </Link>
+      </div>
     </div>
-    
   </nav>
 }
---
+++ src/lib/hooks/useScroll.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+export default function useScroll(){
+    const [scrollPosition, setScrollPosition] = useState(0);
+  
+    useEffect(() => {
+      function handleScroll() {
+        setScrollPosition(window.scrollY);
+      }
+
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [scrollPosition]);
+
+    return  scrollPosition;
+  }
